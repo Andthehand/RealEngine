16261d955dee6b5447a4db88d41ff02b973e689a
Andthehand <sixdefords50@gmail.com>
2021-12-29 22:08:01 -0500

    Added Vertex Buffer Layouts

--- RealEngine/src/RealEngine/Renderer/Buffer.h
+++ RealEngine/src/RealEngine/Renderer/Buffer.h
@@ -1,12 +1,97 @@
 #pragma once
 
 namespace RealEngine {
+	enum class ShaderDataType {
+		None = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
+	};
+
+	static uint32_t ShaderDataTypeSize(ShaderDataType type) {
+		switch (type) {
+			case RealEngine::ShaderDataType::Float:	  return 4;
+			case RealEngine::ShaderDataType::Float2:  return 4 * 2;
+			case RealEngine::ShaderDataType::Float3:  return 4 * 3;
+			case RealEngine::ShaderDataType::Float4:  return 4 * 4;
+			case RealEngine::ShaderDataType::Mat3:	  return 4 * 3 * 3;
+			case RealEngine::ShaderDataType::Mat4:	  return 4 * 4 * 4;
+			case RealEngine::ShaderDataType::Int:	  return 4;
+			case RealEngine::ShaderDataType::Int2:	  return 4 * 2;
+			case RealEngine::ShaderDataType::Int3:	  return 4 * 3;
+			case RealEngine::ShaderDataType::Int4:	  return 4 * 4;
+			case RealEngine::ShaderDataType::Bool:	  return 1;
+		}
+	}
+
+	struct BufferElement {
+		std::string Name;
+		ShaderDataType Type;
+		uint32_t Offset;
+		uint32_t Size;
+		bool Normalized;
+
+		BufferElement() {}
+
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized) { }
+	
+		uint32_t GetComponentCount() const {
+			switch (Type) {
+				case RealEngine::ShaderDataType::Float:  return 1;
+				case RealEngine::ShaderDataType::Float2: return 2;
+				case RealEngine::ShaderDataType::Float3: return 3;
+				case RealEngine::ShaderDataType::Float4: return 4;
+				case RealEngine::ShaderDataType::Mat3:	 return 3 * 3;
+				case RealEngine::ShaderDataType::Mat4:	 return 4 * 4;
+				case RealEngine::ShaderDataType::Int:	 return 1;
+				case RealEngine::ShaderDataType::Int2:	 return 2;
+				case RealEngine::ShaderDataType::Int3:	 return 3;
+				case RealEngine::ShaderDataType::Int4:	 return 4;
+				case RealEngine::ShaderDataType::Bool:	 return 1;
+			}
+
+			RE_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			return 0;
+		}
+	};
+
+	class BufferLayout {
+	public:
+		BufferLayout() {}
+
+		BufferLayout(const std::initializer_list<BufferElement>& elements) : m_Elements(elements) {
+			CalculateOffsetsAndStride();
+		}
+
+		inline uint32_t GetStride() const { return m_Stride; }
+		inline const std::vector<BufferElement>& GetElements() { return m_Elements; }
+	
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
+	private:
+		void CalculateOffsetsAndStride() {
+			uint32_t offset = 0;
+			m_Stride = 0;
+			for (auto& element : m_Elements) {
+				element.Offset = offset;
+				offset += element.Size;
+				m_Stride += element.Size;
+			}
+		}
+
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
+	};
+
 	class VertexBuffer {
 	public:
 		virtual ~VertexBuffer() {}
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
+
+		virtual const BufferLayout& GetLayout() const = 0;
+		virtual void SetLayout(const BufferLayout& layout) = 0;
 
 		static VertexBuffer* Create(float* verticies, uint32_t size);
 	};
