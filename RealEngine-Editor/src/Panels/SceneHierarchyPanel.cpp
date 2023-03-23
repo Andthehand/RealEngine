4112af05d1aca6e59f0f4f32dfda88d1e941aba9
Andthehand <sixdefords50@gmail.com>
2023-03-22 23:41:20 -0400

    Changed both collider components default change speed for offset and size/radius

--- RealEngine-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ RealEngine-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -370,8 +370,8 @@ namespace RealEngine {
 		});
 
 		DrawComponent<BoxCollider2DComponent>("Box Collider 2D", entity, [](auto& component) {
-			ImGui::DragFloat2("Offset", glm::value_ptr(component.Offset));
-			ImGui::DragFloat2("Size", glm::value_ptr(component.Size));
+			ImGui::DragFloat2("Offset", glm::value_ptr(component.Offset), 0.01f);
+			ImGui::DragFloat2("Size", glm::value_ptr(component.Size), 0.01f);
 			ImGui::DragFloat("Density", &component.Density, 0.01f, 0.0f, 1.0f);
 			ImGui::DragFloat("Friction", &component.Friction, 0.01f, 0.0f, 1.0f);
 			ImGui::DragFloat("Restitution", &component.Restitution, 0.01f, 0.0f, 1.0f);
@@ -379,8 +379,8 @@ namespace RealEngine {
 		});
 
 		DrawComponent<CircleCollider2DComponent>("Circle Collider 2D", entity, [](auto& component) {
-			ImGui::DragFloat2("Offset", glm::value_ptr(component.Offset));
-			ImGui::DragFloat("Radius", &component.Radius);
+			ImGui::DragFloat2("Offset", glm::value_ptr(component.Offset), 0.01f);
+			ImGui::DragFloat("Radius", &component.Radius, 0.01f);
 			ImGui::DragFloat("Density", &component.Density, 0.01f, 0.0f, 1.0f);
 			ImGui::DragFloat("Friction", &component.Friction, 0.01f, 0.0f, 1.0f);
 			ImGui::DragFloat("Restitution", &component.Restitution, 0.01f, 0.0f, 1.0f);
