df036743433469a66a8a0dbaa81154e84152766a
Andthehand <sixdefords50@gmail.com>
2023-11-19 14:12:53 -0500

    Changed font size for redability

--- RealEngine/src/RealEngine/ImGui/ImGuiLayer.cpp
+++ RealEngine/src/RealEngine/ImGui/ImGuiLayer.cpp
@@ -40,7 +40,7 @@ namespace RealEngine {
 		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
 		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
 
-		float fontSize = 18.0f;
+		float fontSize = 32.0f;
 		io.Fonts->AddFontFromFileTTF("assets/fonts/opensans/OpenSans-Bold.ttf", fontSize);
 		io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/opensans/OpenSans-Regular.ttf", fontSize);
 
