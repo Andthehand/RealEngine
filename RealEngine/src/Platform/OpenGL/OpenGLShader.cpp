f238acb0fe66e8e54444652b352af04518683748
Andthehand <sixdefords50@gmail.com>
2023-12-03 00:04:41 -0500

    Added back "push_constants"

--- RealEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ RealEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -423,4 +423,53 @@ namespace RealEngine {
 	void OpenGLShader::Unbind() const {
 		glUseProgram(0);
 	}
+
+	void OpenGLShader::SetInt(const std::string& name, int value) {
+		RE_PROFILE_FUNCTION();
+
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count) {
+		RE_PROFILE_FUNCTION();
+
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location, count, values);
+	}
+
+	void OpenGLShader::SetFloat(const std::string& name, float value) {
+		RE_PROFILE_FUNCTION();
+
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1f(location, value);
+	}
+
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value) {
+		RE_PROFILE_FUNCTION();
+
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform2f(location, value.x, value.y);
+	}
+
+	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value) {
+		RE_PROFILE_FUNCTION();
+
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform3f(location, value.x, value.y, value.z);
+	}
+
+	void OpenGLShader::SetFloat4(const std::string& name, const glm::vec4& value) {
+		RE_PROFILE_FUNCTION();
+
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform4f(location, value.x, value.y, value.z, value.w);
+	}
+
+	void OpenGLShader::SetMat4(const std::string& name, const glm::mat4& matrix) {
+		RE_PROFILE_FUNCTION();
+
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+	}
 }
